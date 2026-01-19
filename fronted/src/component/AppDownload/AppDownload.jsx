444fb9872db31c129ff4c58fa8ec7606db45a5d9
Jubair02 <104502849+Jubair02@users.noreply.github.com>
2026-01-20 01:04:10 +0600

    final commit

--- fronted/src/component/AppDownload/AppDownload.jsx
+++ fronted/src/component/AppDownload/AppDownload.jsx
@@ -1,15 +1,15 @@
 import React from 'react'
 import './AppDownload.css'
 import { assets } from '../../assets/assets'
+
 const AppDownload = () => {
   return (
     <div className='app-download' id='app-download'>
-        <p>For Better Experience Download <br /> KhaiDai </p>
+        <p>For Better Experience Download <br /> <span className="highlight">KhaiDai App</span></p>
         <div className="app-download-platforms">
-            <img src={assets.play_store} alt="" />
-            <img src={assets.app_store} alt="" />
+            <img src={assets.play_store} alt="Google Play Store" />
+            <img src={assets.app_store} alt="Apple App Store" />
         </div>
-      
     </div>
   )
 }
--- fronted/src/component/Footer/Footer.jsx
+++ fronted/src/component/Footer/Footer.jsx
@@ -1,48 +1,52 @@
 import React from 'react'
 import './Footer.css'
 import { assets } from '../../assets/assets'
+
 const Footer = () => {
-    return (
-        <div className='footer' id='footer'>
-            <div className="footer-content">
-                <div className="footer-content-left">
-                    <img src={assets.logo} alt="" />
-                    <p> Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sunt sapiente enim distinctio earum, nemo facilis, libero eligendi adipisci possimus tempore nesciunt quae mollitia quidem. Similique nam quo perferendis optio facere.</p>
-                    <div className="footer-social-icons">
-                        <img src={assets.facebook_icon} alt="" />
-                        <img src={assets.twitter_icon} alt="" />
-                        <img src={assets.linkedin_icon} alt="" />
-                    </div>
-                </div>
-
-                <div className='footer-content-center'>
-                    <h2>Company</h2>
-                    <ul>
-                        <li>Home</li>
-                        <li>About Us</li>
-                        <li>Delivery</li>
-                        <li>Privacy policy</li>
-
-                    </ul>
-                </div>
-
-                <div className="footer-content-right">
-                <h2>GET IN TOUCH</h2>
-                <ul>
-                    <li>01756-456822</li>
-                    <li>contact@khaidai.com</li>
-                </ul>
-                </div>
-
-                
-            </div>
-            <hr />
-            <p className="footer-copyright">
-                &copy; 2024 Khaidai.com - All rights reserved.
-            </p>
-
+  return (
+    <div className='footer' id='footer'>
+      <div className="footer-content">
+        
+        {/* Left Section: Brand & Bio */}
+        <div className="footer-content-left">
+          <img src={assets.logo} alt="Logo" className="footer-logo" />
+          <p>Order your favorite meals with ease. We bring the best local flavors directly to your doorstep with speed and care.</p>
+          <div className="footer-social-icons">
+            <img src={assets.facebook_icon} alt="Facebook" />
+            <img src={assets.twitter_icon} alt="Twitter" />
+            <img src={assets.linkedin_icon} alt="LinkedIn" />
+          </div>
         </div>
-    )
+
+        {/* Center Section: Navigation */}
+        <div className='footer-content-center'>
+          <h2>COMPANY</h2>
+          <ul>
+            <li>Home</li>
+            <li>About Us</li>
+            <li>Delivery</li>
+            <li>Privacy Policy</li>
+          </ul>
+        </div>
+
+        {/* Right Section: Contact */}
+        <div className="footer-content-right">
+          <h2>GET IN TOUCH</h2>
+          <ul>
+            <li>+880 1756-456822</li>
+            <li>contact@khaidai.com</li>
+          </ul>
+        </div>
+
+      </div>
+      
+      <hr />
+      
+      <p className="footer-copyright">
+        Copyright 2024 © Khaidai.com - All Rights Reserved.
+      </p>
+    </div>
+  )
 }
 
 export default Footer
